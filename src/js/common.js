65652ee6c5f84cc0833b15cd939228c2be4eadf2
kyle <kimsm0408@naver.com>
2021-09-17 02:37:15 +0900

    2.5.5 update

--- src/js/common.js
+++ src/js/common.js
@@ -1832,6 +1832,33 @@ function commonForScroll() {
     
 }
 
+function refreshAds(type) {
+    if(type == 'init') {
+        refreshAds('recommend');
+        setTimeout(function() {
+            refreshAds('sidebar');
+        },200);
+    } else {
+        setTimeout(function() {
+            if(type == 'recommend') {
+                if($('#recommend-ads > ins').children().length > 0) {
+                    $('#recommend-ads > ins').children().remove();
+                    $('#recommend-ads > ins').removeAttr('data-adsbygoogle-status');
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                    refreshAds('recommend');
+                }
+            } else if(type == 'sidebar') {
+                if($('.sidebar .revenue_unit_wrap .adsbygoogle').length > 0) {
+                    $('.sidebar .revenue_unit_wrap .adsbygoogle').children().remove();
+                    $('.sidebar .revenue_unit_wrap .adsbygoogle').removeAttr('data-adsbygoogle-status');
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                    refreshAds('sidebar');
+                }
+            }
+        }, 30000*(Math.random()*2+1));
+    }
+}
+
 $(document).ready(function() {
     makeToc();
     selectMakeFloatingToc();
@@ -1847,6 +1874,7 @@ $(document).ready(function() {
     bgmEvents();
     toggleCategory();
     replaceLink();
+    refreshAds('init');
     common();
     
     $(window).on('scroll resize', function() {
